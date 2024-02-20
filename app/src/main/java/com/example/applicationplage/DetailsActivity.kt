8d7716e811bef6d4488216d2fbe28bab05b401dc
Nathan <nathanlgd14@gmail.com>
2024-02-20 12:20:01 +0100

    Vue d'une plage + fonction binding

--- app/src/main/java/com/example/applicationplage/DetailsActivity.kt
+++ app/src/main/java/com/example/applicationplage/DetailsActivity.kt
@@ -2,10 +2,39 @@ package com.example.applicationplage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.applicationplage.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
+
+    private val TAG="DetailsActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        //setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mapButton.setOnClickListener {
+            Toast.makeText(this,
+                getString(R.string.aller_vers_la_carte_des_plages),
+                Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "Aller vers les cartes")
+
+
+            finish()
+        }
+
+        binding.beachButton.setOnClickListener {
+            Toast.makeText(this,
+                getString(R.string.aller_vers_plages),
+                Toast.LENGTH_SHORT).show()
+
+
+            finish()
+        }
+
     }
 }
